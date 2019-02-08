2ae8b2843d63573799f305c7c710f8d4684026fe
GermainJonathan <jgermain@gismartware.com>
2019-02-08 17:38:07 +0100

    :tada: Debut du Tp

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 const $ = require("jquery");
 require("materialize-css");
+
+fetch('https://jsonplaceholder.typicode.com/users/1')
+  .then(response => response.json())
+  .then(json => {
+    console.log(json);
+  });
