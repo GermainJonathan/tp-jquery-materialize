2a4459815aec0044346df18feeb761ee1190eca0
GermainJonathan <jgermain@gismartware.com>
2019-02-11 14:32:44 +0100

    :construction: J'avance j'avance

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const $ = require("jquery");
 require("materialize-css");
-
-fetch('https://jsonplaceholder.typicode.com/users/1')
+M.AutoInit();
+fetch("https://jsonplaceholder.typicode.com/users/1")
   .then(response => response.json())
   .then(json => {
     console.log(json);
+    $(".card-content > span").append(json.name);
+    $(".card-reveal > span").append(json.name);
+    $.each(json, function(index, value) {
+      if ($("div.description ." + index).length != 0) {
+        $("div.description ." + index).append(value);
+      }
+      if(index === "address") {
+        $.each(json.address, function(index, value) {
+          if ($("#address ." + index).length != 0) {
+            $("#address ." + index).append(value);
+          }
+          if(index === "geo") {
+            $("#address .link").href = $("#address .link").href + value.lat + "," + value.lng;
+          }
+        });
+      }
+    });
+  });
+fetch("https://jsonplaceholder.typicode.com/photos/1")
+  .then(response => response.json())
+  .then(json => {
+    $("img").attr('src', json.thumbnailUrl);
   });
